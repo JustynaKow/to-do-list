aefdccd7e8044d46b68f465904d81ba146e6ee7a
Justyna Kowalewska <justynajaglowska1@gmail.com>
2023-03-13 10:56:37 +0100

    adding pointer cursor to buttons

--- JS/script.js
+++ JS/script.js
@@ -51,13 +51,13 @@
             <li
             class="tasks__item js-task"
             >
-            <button class="tasks__button tasks__button--toggleDone js-toggleDone">
+            <button class="button tasks__button tasks__button--toggleDone js-toggleDone">
             ${task.done ? "✓" : ""}
             </button>
             <span class="tasks__content${task.done ? " tasks__content--done" : ""}">
             ${task.content}
             </span>
-            <button class="tasks__button tasks__button--remove js-remove">
+            <button class="button tasks__button tasks__button--remove js-remove">
             🗑
             </button>
             </li >
