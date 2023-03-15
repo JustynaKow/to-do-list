dbe15e8006f117b4c76d818eed1b15ced7fc937f
Justyna Kowalewska <justynajaglowska1@gmail.com>
2023-03-15 14:46:54 +0100

    refactor js with immutability

--- JS/script.js
+++ JS/script.js
@@ -3,23 +3,35 @@
         console.log("Cześć! Stwórz swoją listę zadań!");
     };
 
-    const tasks = [];
+    let tasks = [];
 
     const removeTask = (taskIndex) => {
-        tasks.splice(taskIndex, 1);
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            ...tasks.slice(taskIndex+1),
+        ];
+        
         render();
     };
 
     const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        });
-
+        tasks = [
+            ...tasks,
+            {content: newTaskContent},
+           ];
         render();
     };
 
     const toggleTaskDone = (taskIndex) => {
-        tasks[taskIndex].done = !tasks[taskIndex].done;
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            {
+                ...tasks[taskIndex],
+                done: !tasks[taskIndex].done,
+            },
+            ...tasks.slice(taskIndex+1),
+        ];
+        
         render();
     };
 
